1667ae15cb0d71303685423547ca807ad3434138
Rouncer27 <trevor@switchbackcreative.ca>
2021-07-16 10:50:19 -0600

    added the events list component

--- src/components/PageComponents/EventsList.js
+++ src/components/PageComponents/EventsList.js
@@ -1,11 +1,209 @@
 import React from "react"
+import styled from "styled-components"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import {
+  colors,
+  H2Brown,
+  medWrapper,
+  fontSizer,
+  fonts,
+  B1Brown,
+  Btn1GoldRev,
+} from "../../styles/helpers"
+
+const getData = graphql`
+  {
+    events: allWpResolveEvents {
+      edges {
+        node {
+          slug
+          id
+          title
+          acfEvents {
+            dateTime
+            excerpt
+            fieldGroupName
+            fullDetails
+            location
+            featuredImage {
+              altText
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(width: 1000)
+                }
+              }
+            }
+          }
+
+          eventCategories {
+            nodes {
+              slug
+              termTaxonomyId
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const EventsList = ({ data }) => {
+  const eventData = useStaticQuery(getData)
+  const events = eventData.events.edges
+  if (!data.display) return null
+
+  console.log(events)
+
   return (
-    <div>
-      <h1>Events List</h1>
-    </div>
+    <EventsListSection>
+      <div className="wrapper">
+        {events.map(event => {
+          const months = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sept",
+            "Oct",
+            "Nov",
+            "Dec",
+          ]
+
+          const dateArray = event.node.acfEvents.dateTime
+            .split("/")
+            .join(" ")
+            .split(" ")
+
+          const dbDay = parseInt(dateArray[0])
+          const dbMonth = parseInt(dateArray[1])
+          const dbYear = parseInt(dateArray[2])
+          const monthName = months[dbMonth]
+
+          return (
+            <ListItem key={event.node.id}>
+              <div className="date">
+                <p>
+                  <span>{monthName}.</span>
+                  <span className="big-number">{dbDay}</span>
+                </p>
+              </div>
+              <div className="content">
+                <div className="content__title">
+                  <h2>{event.node.title}</h2>
+                </div>
+                <div className="content__location">
+                  <p>
+                    <span>Location: </span>
+                    {event.node.acfEvents.location}
+                  </p>
+                  <p>
+                    <span>Time: </span>
+                    {event.node.acfEvents.dateTime}
+                  </p>
+                </div>
+                <div
+                  className="content__excerpt"
+                  dangerouslySetInnerHTML={{
+                    __html: event.node.acfEvents.excerpt,
+                  }}
+                />
+                <div className="content__button">
+                  <Link to={`/events/${event.node.slug}`}>
+                    More Information
+                  </Link>
+                </div>
+              </div>
+            </ListItem>
+          )
+        })}
+      </div>
+    </EventsListSection>
   )
 }
+
+const EventsListSection = styled.section`
+  .wrapper {
+    ${medWrapper};
+  }
+`
+
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  margin: 3rem 0;
+  padding: 2rem;
+  border-top: solid 0.25rem ${colors.colorTertiary};
+
+  .date {
+    width: 25%;
+    text-align: center;
+
+    p {
+      ${H2Brown};
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin: 0;
+      font-weight: normal;
+      color: ${colors.colorPrimary};
+
+      span {
+        display: block;
+        margin: 0;
+      }
+
+      .big-number {
+        ${fontSizer(5, 9, 76.8, 150, 3.0)};
+        font-family: ${fonts.fontSecondary};
+      }
+    }
+  }
+
+  .content {
+    width: 75%;
+
+    &__title {
+      h2 {
+        ${H2Brown};
+        font-family: ${fonts.fontSecondary};
+      }
+    }
+
+    &__location {
+      padding-bottom: 2.5rem;
+
+      p {
+        ${B1Brown};
+        ${fontSizer(1.4, 1.6, 76.8, 150, 1.8)};
+        margin: 0;
+
+        span {
+          text-transform: uppercase;
+        }
+      }
+    }
+
+    &__excerpt {
+      p {
+        ${B1Brown};
+        ${fontSizer(1.4, 1.6, 76.8, 150, 1.8)};
+      }
+    }
+
+    &__button {
+      a {
+        ${Btn1GoldRev};
+      }
+    }
+  }
+`
 
 export default EventsList
