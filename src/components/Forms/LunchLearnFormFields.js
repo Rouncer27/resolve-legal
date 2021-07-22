7168a8a37de2d3d8122a03a6b480116175bac914
Rouncer27 <trevor@switchbackcreative.ca>
2021-07-22 09:37:22 -0600

    styles for the lunch and learn form

--- src/components/Forms/LunchLearnFormFields.js
+++ src/components/Forms/LunchLearnFormFields.js
@@ -476,7 +476,8 @@ const InputField = styled.div`
     }
 
     input,
-    textarea {
+    textarea,
+    select {
       display: block;
       margin-top: 0.25rem;
       margin-bottom: 0.5rem;
@@ -487,6 +488,31 @@ const InputField = styled.div`
       margin-right: 0;
       width: 100%;
       border: 0.1rem ${colors.colorSecondary} solid;
+    }
+
+    .react-datepicker-wrapper {
+      display: block;
+    }
+
+    .react-datepicker {
+      font-size: 1.3rem !important;
+    }
+
+    .react-datepicker__current-month {
+      font-size: 1.5rem !important;
+    }
+
+    .react-datepicker__header {
+      padding-top: 6px !important;
+    }
+
+    .react-datepicker__navigation {
+      top: 13px !important;
+    }
+
+    .react-datepicker__day-name,
+    .react-datepicker__day {
+      margin: 0.5rem !important;
     }
   }
 `
