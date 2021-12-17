629801dbe1c44f0c68101831699e30cb3951c201
Rouncer27 <trevor@switchbackcreative.ca>
2021-12-17 10:37:18 -0700

    redirects and cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,26 @@ module.exports = {
         siteUrl: `https://www.resolvelegalgroup.com/`,
       },
     },
-    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeSecurityHeaders: false,
+        headers: {
+          "/*": [
+            "cache-control: public,max-age=60",
+            "X-Frame-Options: sameorigin",
+            "X-XSS-Protection: 1; mode=block",
+            "X-Content-Type-Options: nosniff",
+            "Feature-Policy: camera 'none'; geolocation 'none'; microphone 'none'",
+          ],
+          "/**/*.html": ["cache-control: public, max-age=300"],
+          "/static/*": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.js": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.css": ["cache-control: public, max-age=31536000, immutable"],
+          "/sw.js": ["cache-control: public, max-age=0, must-revalidate"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
