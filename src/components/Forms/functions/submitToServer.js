5a67f23948f1bce21714cbf3f443d8d54964cfc4
Trevor Rounce <trevor@switchbackcreative.ca>
2025-08-26 10:59:54 -0600

    fix server

--- src/components/Forms/functions/submitToServer.js
+++ src/components/Forms/functions/submitToServer.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const submitToWebServer = async (formID, data) => {
-  const FORM_POST_URL = `https://resolvedb.swbcreative.ca/wp-json/contact-form-7/v1/contact-forms/${formID}/feedback`
+  const FORM_POST_URL = `https://backenddb.resolvelegalgroup.com/wp-json/contact-form-7/v1/contact-forms/${formID}/feedback`
   const config = { headers: { "Content-Type": "multipart/form-data" } }
   const serverResponse = await axios.post(FORM_POST_URL, data, config)
 
